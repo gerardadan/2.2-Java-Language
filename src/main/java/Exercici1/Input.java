d3e61f160864c2a9609d1bd878b4df136ab138be
geridev <publiger30@gmail.com>
2025-03-12 11:03:59 +0100

    Ex 2-corrections

--- src/main/java/Exercici1/Input.java
+++ src/main/java/Exercici1/Input.java
@@ -5,55 +5,132 @@ import java.util.Scanner;
 
 public class Input {
     static Scanner scanner = new Scanner(System.in);
-
+    static final String errorMessage = "Error: the value is not a ";
+    
     public static byte readByte(String message) {
         while (true){
             try {
                 printMessage(message);
                 byte value = scanner.nextByte();
-                Input.scanner.nextLine();
+                scanner.nextLine();
+
                 return value;
             } catch (InputMismatchException e) {
-                System.out.println("Error input it not byte");
-                Input.scanner.nextLine();
+                printMessage(errorMessage + "byte");
+                scanner.nextLine();
             }
         }
     }
 
-    public static int readInt(String message) throws InputMismatchException{
-        printMessage(message);
-        return scanner.nextInt();
+    public static int readInt(String message) {
+        while (true){
+            try {
+                printMessage(message);
+                int value = scanner.nextInt();
+                scanner.nextLine();
+
+                return value;
+            } catch (InputMismatchException e) {
+                printMessage(errorMessage + "Int");
+                scanner.nextLine();
+            }
+        }
     }
 
-    public static Float readFloat(String message)throws InputMismatchException{
-        printMessage(message);
-        return scanner.nextFloat();
+    public static float readFloat(String message) {
+        while (true){
+            try {
+                printMessage(message);
+                float value = scanner.nextFloat();
+                scanner.nextLine();
+
+                return value;
+            } catch (InputMismatchException e) {
+                printMessage(errorMessage + "Float");
+                scanner.nextLine();
+            }
+        }
     }
 
-    public static double readDouble(String message)throws InputMismatchException{
-        printMessage(message);
-        return scanner.nextDouble();
+    public static double readDouble(String message) {
+        while (true){
+            try {
+                printMessage(message);
+                double value = scanner.nextDouble();
+                scanner.nextLine();
+
+                return value;
+            } catch (InputMismatchException e) {
+                printMessage(errorMessage + "Double");
+                scanner.nextLine();
+            }
+        }
     }
 
-    public static char readChar(String message)throws Exception{
-        printMessage(message);
-        if(scanner.next().length() > 1)
-            throw new Exception("Value out of range");
-        return scanner.next().charAt(0);
+    public static char readChar(String message) {
+        while (true){
+            try {
+                printMessage(message);
+                String value = scanner.next();
+                if(value.length() > 1)
+                    throw new Exception("Value out of range");
+                scanner.nextLine();
+
+                return value.charAt(0);
+            } catch (Exception e) {
+                printMessage(errorMessage + "Char");
+                scanner.nextLine();
+            }
+        }
     }
 
-    public static String readString(String message)throws Exception{
-        printMessage(message);
-        if(scanner.nextLine().isEmpty())
-            throw new Exception("Empty value");
-        return scanner.nextLine();
+    public static String readString(String message) {
+        while (true){
+            try {
+                printMessage(message);
+                String value = scanner.next();
+                if(value.isEmpty())
+                    throw new Exception("Empty value");
+                scanner.nextLine();
+
+                return value;
+            } catch (Exception e) {
+                printMessage(errorMessage + "String");
+                scanner.nextLine();
+            }
+        }
     }
 
-    public static Boolean readBoolean(String message)throws Exception{
-        printMessage(message);
-        return scanner.nextBoolean();
+    public static Boolean readBoolean(String message) {
+        while (true){
+            try {
+                printMessage(message);
+                Boolean value = scanner.nextBoolean();
+                scanner.nextLine();
+
+                return value;
+            } catch (Exception e) {
+                printMessage(errorMessage + "Boolean");
+                scanner.nextLine();
+            }
+        }
     }
 
+    public static Boolean readCustomBoolean(String message) {
+        while (true){
+            try {
+                char charValue = readChar(message);
+                if(charValue == 's'){
+                    return true;
+                } else if (charValue != 'n') {
+                    throw new Exception("Please respond with 's' for True or 'n' for False");
+                }
+            } catch (Exception e) {
+                printMessage(e.getMessage());
+            }
+        }
+    }
+    
     private static void printMessage(String message) {
         System.out.println(message);
     }
--- src/main/java/Exercici1/Main.java
+++ src/main/java/Exercici1/Main.java
@@ -1,79 +1,16 @@
 package Exercici1;
 
-import java.util.InputMismatchException;
-import java.util.Scanner;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         Input.readByte("Set byte");
-        readChar("Set letter");
-
+        Input.readInt("Set Int");
+        Input.readFloat("Set Float");
+        Input.readDouble("Set Double");
+        Input.readChar("Set Char");
+        Input.readString("Set String");
+        Input.readBoolean("Set Boolean");
+        Input.readCustomBoolean("Set 's' for True or 'n' for false");
         Input.close();
-        System.out.println("Fin");
     }
-
-    public static int readInt(String message) {
-        try {
-            return Input.readInt(message);
-        } catch (InputMismatchException ime) {
-            System.out.println("Error Int");
-            Input.scanner.nextLine();
-            return readInt(message);
-        }
-    }
-
-    public static double readDouble(String message) {
-        try {
-            return Input.readDouble(message);
-        } catch (InputMismatchException ime) {
-            System.out.println("Error double");
-            Input.scanner.nextLine();
-            return readDouble(message);
-        }
-    }
-
-    public static float readFloat(String message) {
-        try {
-            return Input.readFloat(message);
-        } catch (InputMismatchException ime) {
-            System.out.println("Error Float");
-            Input.scanner.nextLine();
-            return readFloat(message);
-        }
-    }
-
-    public static char readChar(String message) {
-        try {
-            return Input.readChar(message);
-        } catch (Exception e) {
-            System.out.println("Error char");
-            Input.scanner.nextLine();
-            return readChar(message);
-        }
-    }
-
-    public static String readString(String message) {
-        try {
-            return Input.readString(message);
-        } catch (Exception e) {
-            System.out.println("Error String");
-            Input.scanner.nextLine();
-            return readString(message);
-        }
-    }
-
-    public static boolean readBoolean(String message) {
-        try {
-            return Input.readBoolean(message);
-        } catch (Exception e) {
-            System.out.println("Error boolean");
-            Input.scanner.nextLine();
-            return readBoolean(message);
-        }
-    }
-
 }
